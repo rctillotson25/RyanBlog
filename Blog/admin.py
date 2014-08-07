5d61a8a3a7e11b40bffe36033204f8ae5fc1732d
rctillotson25 <rctillotson25@gmail.com>
2014-08-06 19:49:59 -0500

    Modified the view for the admin Page and implemented a detail view

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from Blog.models import Post, PostCategory
+from django.forms import TextInput, Textarea
+from django.db import models
+
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
 	list_display = ('title', 'date')
 	list_filter = ['date']
 	search_fields = ['title','body', 'body2']
-	
+	formfield_overrides = {
+		models.CharField: {'widget': Textarea(attrs={'rows':5, 'cols':40})},
+		models.TextField: {'widget': Textarea(attrs={'rows':25, 'cols':40})},
+	}	
 admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
--- Blog/views.py
+++ Blog/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 from Blog.models import Post
 
 # Create your views here.
 def index(request):
 	latest_posts_list = Post.objects.order_by('-date')[:5]
-	template = loader.get_template('blog/index.html')
-	context = RequestContext(request, {
-		'latest_posts_list': latest_posts_list,
-		})
-	return HttpResponse(template.render(context))
+	context = {'latest_posts_list': latest_posts_list}
+	return render(request, 'blog/index.html', context)
 	
 def detail(request, post_id):
-	return HttpResponse("You're looking at the post %s." % post_id)
+	try:
+		post = Post.objects.get(pk=post_id)
+	except Post.DoesNotExist:
+		raise Http404
+	return render(request, 'blog/detail.html', {'post' : post})
 	
 def results(request, post_id):
 	return HttpResponse("You're looking at the results of post %s." % post_id)
